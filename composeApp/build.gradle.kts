9691e3c54b76a735f77dd1121815628fd6540496
weix2025 <git>
2025-07-10 09:35:58 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   composeApp/build.gradle.kts
    	new file:   composeApp/lint-baseline.xml
    	modified:   gradle/libs.versions.toml

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -106,6 +106,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    lint {
+        baseline = file("lint-baseline.xml")
+    }
 }
 
 dependencies {
